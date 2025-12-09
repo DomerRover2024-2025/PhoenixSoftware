ba13cefd730df32e705905dbc115e7feebec69d6
henryJ099123 <henryjochan@gmail.com>
2025-12-09 18:47:35 -0500

    Small issues with serialReaderWriter

--- radio_comms/hieroglyphics/serialReaderWriter.py
+++ radio_comms/hieroglyphics/serialReaderWriter.py
@@ -9,6 +9,7 @@ class SerialReaderWriter(ReaderWriter):
         self.ser : Serial = Serial(port=port, baudrate=baud, timeout=timeout)
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
+        self.messageQueue = messageQueue
 
     def readBytes(self, num_bytes : int):
         read_bytes = b''
@@ -21,25 +22,25 @@ class SerialReaderWriter(ReaderWriter):
     def readMessage(self) -> Message:
         potentialMessage = Message(new=False)
 
-        b_id = readBytes(2)
+        b_id = self.readBytes(2)
         potentialMessage.set_msg_id(struct.unpack(">H", b_id)[0])
 
-        b_purpose = readBytes(1)
+        b_purpose = self.readBytes(1)
         potentialMessage.set_purpose(struct.unpack(">B", b_purpose)[0])
 
-        b_number = readBytes(1)
+        b_number = self.readBytes(1)
         potentialMessage.number = struct.unpack(">B", b_number)[0]
 
-        b_size = readBytes(4)
+        b_size = self.readBytes(4)
         potentialMessage.set_size(struct.unpack(">L", b_size)[0])
 
         if potentialMessage.size_of_payload > 4096:
             print(f"--Error: buffer? ID: {potentialMessage.msg_id}, purpose: {potentialMessage.purpose}, num: {potentialMessage.number}, len_payload: {potentialMessage.size_of_payload}")
 
-        payload = readBytes(potentialMessage.size_of_payload)
+        payload = self.readBytes(potentialMessage.size_of_payload)
         potentialMessage.set_payload(payload)
 
-        checksum = readBytes(1)
+        checksum = self.readBytes(1)
         same_checksums, calculated_checksum = Message.test_checksum(bytestring=potentialMessage.get_as_bytes()[:-1], checksum=checksum)
 
         if not same_checksums:    
